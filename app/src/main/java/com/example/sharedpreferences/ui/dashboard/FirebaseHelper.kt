ecb6a8c027b736f3919bfa167ea18b24a38e882c
bqmolina <bqmolina@up.edu.ph>
2025-01-05 15:49:43 +0800

    Implement new Dashboard UI using a single fragment instead of toggling between 2 fragments.

--- app/src/main/java/com/example/sharedpreferences/ui/dashboard/FirebaseHelper.kt
+++ app/src/main/java/com/example/sharedpreferences/ui/dashboard/FirebaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.sharedpreferences.firebase
 
+import android.widget.*
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 import java.text.SimpleDateFormat
@@ -77,11 +78,11 @@ class FirebaseHelper {
         val collectionName = "Measurements"
 
         val newStudentMeasurement = hashMapOf(
-            "Name" to name,
-            "Student Number" to studentNumber,
-            "Height" to height,
-            "Weight" to weight,
-            "Timestamp" to timestamp
+            "name" to name,
+            "lrn" to studentNumber,
+            "height" to height,
+            "weight" to weight,
+            "timestamp" to timestamp
         )
 
         firestore.collection(collectionName).add(newStudentMeasurement).await()
@@ -105,16 +106,15 @@ class FirebaseHelper {
 
     suspend fun getStudentsFromMeasurementsCollection(): List<StudentMeasurement> {
         val snapshot = firestore.collection("Measurements")
-            .orderBy("Timestamp")
             .get()
             .await()
 
         return snapshot.documents.mapNotNull {
-            val name = it.getString("Name")
-            val studentNumber = it.getString("Student Number")
-            val height = it.getDouble("Height")?.toFloat()
-            val weight = it.getDouble("Weight")?.toFloat()
-            val timestamp = it.getString("Timestamp")
+            val name = it.getString("name")
+            val studentNumber = it.getString("lrn")
+            val height = it.getDouble("height")?.toFloat()
+            val weight = it.getDouble("weight")?.toFloat()
+            val timestamp = it.getString("timestamp")
             if (name != null && studentNumber != null && timestamp != null) {
                 StudentMeasurement(name, studentNumber, height, weight, timestamp)
             } else null
